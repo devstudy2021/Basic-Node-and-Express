2ee141bc2156a523b897d2015a331b32b55e6367
devstudy2021 <82985260+devstudy2021@users.noreply.github.com>
2023-02-05 19:11:09 -0800

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     res.send("Hello Express");
 }); */
 
+app.use((req, res, next) => {
+    console.log(req.method+" "+req.path+" - "+req.ip);
+    next();
+});
+
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/views/index.html");
 });
