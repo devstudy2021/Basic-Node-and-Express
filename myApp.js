0452ddf007b62ac4fdec899dec1eba0ceb57ab89
devstudy2021 <82985260+devstudy2021@users.noreply.github.com>
2023-02-08 14:50:48 -0800

    Add files via upload

--- myApp.js
+++ myApp.js
@@ -39,7 +39,11 @@ app.get("/:word/echo", (req, res) => {
     res.json({echo: word});
 });
 
-
+app.get("/name", (req, res) => {
+    let first = req.query.first;
+    let last = req.query.last;
+    res.json({name: first+' '+last})
+});
 
 
 
